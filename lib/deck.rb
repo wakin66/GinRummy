89842cd31a1841a970dbb2d49aaf4e9f90d7332a
William Akin <wakin66@gmail.com>
2021-03-11 00:48:44 -0500

    Initial Commit

---
+++ lib/deck.rb
@@ -0,0 +1,30 @@
+require_relative 'card.rb'
+
+class Deck
+
+    def initialize
+        @cards = Array.new
+        fill_deck
+    end
+
+    def draw_card
+        return cards.pop
+    end
+
+    def shuffle
+        @cards.shuffle!
+    end
+
+    private
+
+    attr_reader :cards
+
+    def fill_deck
+        ["♤","♢","♧","♡"].each do |suit|
+            ["A","2","3","4","5","6","7","8","9","10","J","Q","K"].each do |value|
+                cards << Card.new(suit,value)
+            end
+        end
+    end
+
+end
