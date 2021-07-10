3964b887aba01efbe097281a70cf516027ca9cc8
William Akin <wakin66@gmail.com>
2021-07-10 16:10:02 -0400

    Update to Hand class

--- lib/hand.rb
+++ lib/hand.rb
@@ -2,16 +2,26 @@ require_relative 'card.rb'
 
 class Hand
 
-    def initialize (deck, discard)
+    def initialize (deck, discard, cards = Array.new)
         @deck = deck
         @discard = discard
+        @cards = cards
+        @deadwood = Array.new
     end
 
     private
 
-    attr_reader :deck, :discard
+    attr_reader :deck, :discard, :cards
 
     def big_gin?
+        if cards.length != 11 then (return false) end
+    end
+
+    def gin?
+
+    end
+
+    def knock?
 
     end
 
